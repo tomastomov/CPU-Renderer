9258d25ab8e722dce92522c3f532a8591a42d796
tomastomov@yahoo.com <tomastomov@yahoo.com>
2026-07-28 19:33:54 +0300

    Add precompile headers

---
+++ src/headers/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <format>
+#include <utility>
