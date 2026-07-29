d4a2f3e374bf33880de1fcaaa3758e08f7f99df8
tomastomov@yahoo.com <tomastomov@yahoo.com>
2026-07-29 22:56:33 +0300

    Add first version of drawing a triangle

--- src/headers/Log.h
+++ src/headers/Log.h
@@ -5,7 +5,7 @@
 namespace CPURenderer {
 	template<typename ...Args>
 	inline void Log(std::format_string<Args...> format, Args&& ...args) {
-		std::cout << std::format(format, std::forward<Args>(args)...);
+		std::cout << std::format(format, std::forward<Args>(args)...) << std::endl;
 	}
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,81 @@ static uint32_t GetColorFromARGB(uint8_t a, uint8_t r, uint8_t g, uint8_t b) {
     return ((uint32_t)a << 24) | ((uint32_t)r << 16) | ((uint32_t)g << 8) | (uint32_t)b;
 }
 
+struct Vector2 {
+    float x;
+    float y;
+    
+    double GetLength() {
+        double xSquared = x * x;
+        double ySquared = y * y;
+
+        return std::sqrt(xSquared + ySquared);
+    }
+
+    Vector2 GetNormalized() {
+        double length = GetLength();
+
+        if (length <= 0.01) {
+            CPURenderer::Log("Length of a vector is too small skipping normalization");
+            return *this;
+        }
+
+        return Vector2(x / length, y / length);
+    }
+
+    Vector2 operator-(const Vector2 other) {
+        return Vector2(x - other.x, y - other.y);
+    }
+
+    Vector2 operator*(const float value) {
+        return Vector2(x * value, y * value);
+    }
+};
+
+static void DrawLine(Vector2 a, Vector2 b, uint32_t* frameBuffer, const int WIDTH, const int HEIGHT) {
+    CPURenderer::Log("Processing point x: {}, y: {} and x1: {}, y1: {} ", a.x, a.y, b.x, b.y);
+    Vector2 line = b - a;
+    Vector2 normalizedLine = line.GetNormalized();
+
+    float startX = a.x;
+    float startY = a.y;
+    float endX = b.x;
+    float endY = b.y;
+
+    bool xCondition = normalizedLine.x >= 0.0f ? startX <= endX : startX >= endX;
+    bool yCondition = normalizedLine.y >= 0.0f ? startY <= endY : startY >= endY;
+
+    while (xCondition && yCondition) {
+        startX += normalizedLine.x;
+        startY += normalizedLine.y;
+
+        int frameBufferIndex = std::round(startY) * WIDTH + std::round(startX);
+        if (frameBufferIndex < WIDTH * HEIGHT) {
+            frameBuffer[frameBufferIndex] = GetColorFromARGB(255, 0, 0, 255);
+        }
+        xCondition = normalizedLine.x >= 0.0f ? startX <= endX : startX >= endX;
+        yCondition = normalizedLine.y >= 0.0f ? startY <= endY : startY >= endY;
+        CPURenderer::Log("Drawing pixel point x: {}, y: {} at screen location {}", startX, startY, frameBufferIndex);
+    }
+}
+
+static void DrawTriangle(Vector2 a, Vector2 b, Vector2 c, uint32_t* frameBuffer, int WIDTH, int HEIGHT) {
+    double aLen = a.GetLength();
+    double bLen = b.GetLength();
+    double cLen = c.GetLength();
+
+    if (aLen + bLen <= cLen || aLen + cLen <= bLen || bLen + cLen <= aLen) {
+        CPURenderer::Log("Invalid triangle");
+        return;
+    }
+
+    DrawLine(a, b, frameBuffer, WIDTH, HEIGHT);
+    DrawLine(a, c, frameBuffer, WIDTH, HEIGHT);
+    DrawLine(b, c, frameBuffer, WIDTH, HEIGHT);
+
+    return;
+}
+
 int main() {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         CPURenderer::Log("Failed to init sdl video");
@@ -43,6 +118,14 @@ int main() {
             frameBuffer[i] = GetColorFromARGB(255, 255, 0, 0);
         }
     }
+
+    Vector2 v1 = { 20.0f, 20.0f };
+    Vector2 v2 = { 40.0f, 20.0f };
+    Vector2 v3 = { 30.0f, 60.0f };
+
+    DrawTriangle(v1, v2, v3, frameBuffer, WIDTH, HEIGHT);
+    DrawTriangle(v1 * 10.0f, v2 * 10.0f, v3 * 10.0f, frameBuffer, WIDTH, HEIGHT);
+    DrawTriangle(v1 * 100.0f, v2 * 100.0f, v3 * 100.0f, frameBuffer, WIDTH, HEIGHT);
 
     SDL_UpdateTexture(texture, nullptr, frameBuffer, WIDTH * sizeof(uint32_t));
 
