050db4be4ab31be406e12e0d6d3c25a1425522f7
tomastomov@yahoo.com <tomastomov@yahoo.com>
2026-08-01 00:25:01 +0300

    floor and ceil when drawing pixels

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,14 @@ static uint32_t GetColorFromARGB(uint8_t a, uint8_t r, uint8_t g, uint8_t b) {
 	return ((uint32_t)a << 24) | ((uint32_t)r << 16) | ((uint32_t)g << 8) | (uint32_t)b;
 }
 
+static void DrawPixel(int x, int y, uint32_t* frameBuffer, const int WIDTH, const int HEIGHT) {
+	if (x < WIDTH && y < HEIGHT && x >= 0 && y >= 0) {
+		int frameBufferIndex = y * WIDTH + x;
+
+		frameBuffer[frameBufferIndex] = GetColorFromARGB(255, 0, 0, 255);
+	}
+}
+
 static void DrawLine(Vector2 a, Vector2 b, uint32_t* frameBuffer, const int WIDTH, const int HEIGHT) {
 	CPURenderer::Log("Processing point x: {}, y: {} and x1: {}, y1: {} ", a.x, a.y, b.x, b.y);
 	int processedPoints = 0;
@@ -42,14 +50,10 @@ static void DrawLine(Vector2 a, Vector2 b, uint32_t* frameBuffer, const int WIDT
 		start.x += normalizedLine.x;
 		start.y += normalizedLine.y;
 
-		int roundedX = std::ceil(start.x);
-		int roundedY = HEIGHT - 1 - std::ceil(start.y);
-
-		if (roundedX < WIDTH && roundedY < HEIGHT && roundedX >= 0 && roundedY >= 0) {
-			int frameBufferIndex = roundedY * WIDTH + roundedX;
-
-			frameBuffer[frameBufferIndex] = GetColorFromARGB(255, 0, 0, 255);
-		}
+		DrawPixel(std::ceil(start.x), std::ceil(start.y), frameBuffer, WIDTH, HEIGHT);
+		DrawPixel(std::floor(start.x), std::floor(start.y), frameBuffer, WIDTH, HEIGHT);
+		DrawPixel(std::ceil(start.x), std::floor(start.y), frameBuffer, WIDTH, HEIGHT);
+		DrawPixel(std::floor(start.x), std::ceil(start.y), frameBuffer, WIDTH, HEIGHT);
 
 		xCondition = normalizedLine.x >= 0.0f ? start.x <= end.x: start.x >= end.x;
 		yCondition = normalizedLine.y >= 0.0f ? start.y <= end.y : start.y >= end.y;
@@ -146,9 +150,11 @@ int main() {
 	Vector2 topLeft = { -0.5f, 0.5f };
 	Vector2 bottomRight = { 0.5f, -0.5f };
 
-	Triangle2D triangle = Triangle2D::Create(bottomLeft, bottomRight, topLeft, { WIDTH * 0.5f, HEIGHT * 0.5f }, { 500.0f, 500.0f }, 135.0f);
+	Triangle2D triangle = Triangle2D::Create(bottomLeft, bottomRight, topLeft, { WIDTH * 0.5f, HEIGHT * 0.5f }, { 500.0f, 500.0f }, 0.0f);
+	Triangle2D triangle2 = Triangle2D::Create(bottomLeft, bottomRight, topLeft, { 200.0f, 200.0f }, { 20.0f, 20.0f }, 0.0f);
 
 	DrawTriangle(triangle, frameBuffer, WIDTH, HEIGHT);
+	DrawTriangle(triangle2, frameBuffer, WIDTH, HEIGHT);
 
 	SDL_UpdateTexture(texture, nullptr, frameBuffer, WIDTH * sizeof(uint32_t));
 
